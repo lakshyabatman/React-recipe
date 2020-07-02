5e95057216fbcb86aecaad55b4deb5325798dc2f
Lakshya Khera <lakshya.khera@gmail.com>
2020-07-02 19:55:52 +0530

    Init: Initalised eslint and restructured codebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../assets/style/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
